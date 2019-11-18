427d09f8ac752179210aba925826f849bf794306
Buddhistwang <13240143564@163.com>
2019-11-18 23:24:57 +0800

    增加 增加Markdown自动生成目录和锚点美化

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
-
+import re
 import markdown
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
-
 
 
 
@@ -23,12 +24,27 @@ def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     # 引入markdown
     # extra 本身包含很多基础拓展，而
-    # codehilite 是语法高亮拓展，这为后面的实现代码高亮功能提供基础，而
-    # toc 则允许自动生成目录
-    post.body = markdown.markdown(post.body,
-                                  extensions=[
-                                      'markdown.extensions.extra',
-                                      'markdown.extensions.codehilite',
-                                      'markdown.extensions.toc',
-                                  ])
+    # codehilite 是语法高亮拓展，这为后面的实现代码高亮功能提供基础
+    # toc 允许自动生成目录
+    # post.body = markdown.markdown(post.body,
+    #                               extensions=[
+    #                                   'markdown.extensions.extra',
+    #                                   'markdown.extensions.codehilite',
+    #                                   'markdown.extensions.toc',
+    #                               ])
+
+    md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        # 'markdown.extensions.toc',
+        # 美化锚点
+        TocExtension(slugify=slugify),
+    ])
+
+    post.body = md.convert(post.body)
+    # 判断是是否为空
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    post.toc = m.group(1) if m is not None else ''
     return render(request, 'blog/detail.html', context={'post': post})
+
+
